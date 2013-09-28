7c84c3946b5c14393014098c07c6f8d73aca4e22
Jorge Vargas <vargasjorgeluis@gmail.com>
2013-09-28 14:33:19 -0500

    Finished the tile selector Utility

--- web/imageutil.dart
+++ web/imageutil.dart
@@ -7,8 +7,13 @@ import 'dart:html';
 //System vars
 HtmlDocument _doc;
 CanvasRenderingContext2D _ctx;
+CanvasRenderingContext2D _ctxTile;
 CanvasElement canvas;
+CanvasElement tileCanvas;
 ImageElement image;
+ImageElement imageTile;
+var divX;
+var divY;
 int TILE_SIZE;
 
 void main() {
@@ -17,22 +22,39 @@ void main() {
   _ctx = canvas.getContext("2d");
   canvas.width = 1024;//384;
   canvas.height = 2016;//15096;
+  
   TILE_SIZE = 32;//24;
+  
+  tileCanvas = _doc.query("#tile");
+  _ctxTile = canvas.getContext("2d");
+  tileCanvas.width = TILE_SIZE;
+  tileCanvas.height = TILE_SIZE;
   
   image = new Element.tag('img'); 
   image = _doc.createElement('img'); 
   image.src = 'assets/tileset.png';//'assets/iconset.png';
   image.onLoad.listen((value) => update());
   
+  imageTile = new Element.tag('img'); 
+  imageTile = _doc.createElement('img'); 
+  imageTile.src = 'assets/tileset.png';//'assets/iconset.png';
+  
+  divX = _doc.query('#divX');
+  divY = _doc.query('#divY');
+  
   document.onMouseDown.listen(mousePressed);
-
 }
 
 void mousePressed(MouseEvent e){
   int x = (e.pageX/TILE_SIZE).round() -1;
   int y = (e.pageY/TILE_SIZE).round() -1;
   
-  print('X:'+ x.toString()+ ' Y:'+ y.toString());
+  _ctxTile.clearRect(0, 0, TILE_SIZE, TILE_SIZE);
+  _ctxTile.drawImageToRect(imageTile , new Rect(0, 0, TILE_SIZE, TILE_SIZE), //Rect to paint the image
+      sourceRect: new Rect( x * TILE_SIZE, y* TILE_SIZE, TILE_SIZE, TILE_SIZE)); //Size of the image
+  
+  divX.text = 'X:'+ x.toString();
+  divY.text = 'Y:'+ y.toString();
 }
 
 void update(){
