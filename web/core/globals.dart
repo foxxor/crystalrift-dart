a7296fa1efeef7f47ccfe45657ecc7ae577bea7c
Jorge Vargas <Jorge.Vargas@albelli.com>
2018-05-06 21:23:52 -0500

    Renamed some editor global variables names

--- web/core/globals.dart
+++ web/core/globals.dart
@@ -1,9 +1,6 @@
-/*
-  Copyright (C) 2013 Jorge Vargas <vargasjorgeluis@gmail.com>
-*/
 library globals;
 
-//Engine vars
+// Engine vars
 const int INITIAL_FRAME = 1;
 const int MS_PER_FRAME = 240;
 const int SELECTED_CHAR = 2; // Tile X for main character
@@ -18,57 +15,57 @@ const int MAP_WIDTH_TILES = 70;
 const int CAMERA_HEIGHT_TILES = 16; //DEPRECATED
 const int CAMERA_WIDTH_TILES = 22; //DEPRECATED
 
-//Directions
+// Directions
 const int LEFT = 2;
 const int RIGHT = 3;
 const int UP = 0;
 const int DOWN = 1;
 
-//Tile types
+// Tile types
 const int TILE_SOIL = 0;
 const int TILE_ITEM = 1;
 const int TILE_BUILDING_PASSABLE = 2;
 const int TILE_BUILDING_UNPASSABLE = 3;
 
-//Event types
+// Event types
 const int EVENT_TYPE_MESSAGE = 0;
 const int EVENT_TYPE_ANIMATION = 1;
 const int EVENT_TYPE_FIGHT_ANIMATION = 2;
 
-//Animations
+// Animations
 const int ANIMATION_FRAME_HEIGHT = 195;
 const int ANIMATION_FRAME_WIDTH = 192;
 const int ANIMATION_SPEED = 4;
 
-//Windowsets
+// Windowsets
 const int WINDOWSET_BORDER_PADDING = 9;
 const int WINDOWSET_BG_TILE = 64;
 const int WINDOWSET_FG_TILE = 64;
 const int WINDOWSET_TEXT_PADDING = 20;
 
-//Centering type
+// Centering type
 const int CENTER_TYPE_VERTICAL = 1;
 const int CENTER_TYPE_HORIZONTAL = 2;
 
-//Types of actors
+// Types of actors
 const int ACTOR_BEHAVIOUR_GOOD = 1;
 const int ACTOR_BEHAVIOUR_NEUTRAL = 2;
 const int ACTOR_BEHAVIOUR_FOE = 3;
 
 const int MAX_PATHFINDING_ITERATIONS = 100;
 
-//Types of actors
+// Types of actors
 const int ACTOR_COMBAT_FLEE = 1;
 const int ACTOR_COMBAR_AGGRESIVE = 2;
 const int ACTOR_COMBAT_DEFENSIVE = 3;
 
-//Selection modes for the editor
+// Selection modes for the editor
 const int SINGLE_TILE_SELECTION = 1;
 const int MULTI_TILE_SELECTION = 2;
 
 // Editor map area size in tiles
-const int EDITOR_MAP_TILES_SIZE_WIDTH = 32;
-const int EDITOR_MAP_TILES_SIZE_HEIGHT = 22;
+const int EDITOR_MAP_DEFAULT_TILES_WIDTH = 32;
+const int EDITOR_MAP_DEFAULT_TILES_HEIGHT = 22;
 
 // Type of events
 const int EVENT_TYPE_NOT_SPECIFIED = -1;
